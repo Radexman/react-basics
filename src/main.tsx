09314556d67e92637ad60bd53b89945070187592
Radexman <Borderlandsmaniak@gmail.com>
2024-04-06 23:43:25 +0200

    JSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const element = React.createElement('a', { href: 'https://zrozumiecreact.pl' }, 'Test');
+const element = <div></div>;
 
-const elementInJSX = <a href='https://zrozumiecreact.pl'>Test</a>;
-console.log(<h1>Hello</h1>);
-console.log(element);
-
-const currentDate = new Date();
-
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-
-console.log(hours, minutes);
-
-const timer = <div>{`${hours}:${minutes}`}</div>;
-
-ReactDOM.createRoot(document.getElementById('root')!).render(timer);
+ReactDOM.createRoot(document.getElementById('root')!).render(element);
